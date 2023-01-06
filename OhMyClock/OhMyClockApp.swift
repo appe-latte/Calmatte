47a8edd001015bc2954638119bfe2479c224b578
Appè Latte <51999459+appe-latte@users.noreply.github.com>
2023-01-05 17:34:15 -0700

    Initial Commit

---
+++ OhMyClock/OhMyClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhMyClockApp.swift
+//  OhMyClock
+//
+//  Created by Stanford L. Khumalo on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct OhMyClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
