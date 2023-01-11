7ead84b87e7d8809ae05a488ddb4bc86c5a5a2ec
Appè Latte <51999459+appe-latte@users.noreply.github.com>
2023-01-11 01:18:37 -0700

    tab bar / accent color
    - added tab bar
    - moved code to "MainView" after initially being in ContentView.
    - changed icons
    - created MeditationView & SettingsView

---
+++ OhMyClock/Views/MeditationView.swift
@@ -0,0 +1,20 @@
+//
+//  MeditationView.swift
+//  OhMyClock
+//
+//  Created by Stanford L. Khumalo on 2023-01-11.
+//
+
+import SwiftUI
+
+struct MeditationView: View {
+    var body: some View {
+        Text("7 minute meditation")
+    }
+}
+
+struct MeditationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeditationView()
+    }
+}
---
+++ OhMyClock/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  OhMyClock
+//
+//  Created by Stanford L. Khumalo on 2023-01-11.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("More / Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
